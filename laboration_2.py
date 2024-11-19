40ad5d61931ee13a469b29a7ea38be148b9ff017
NilsFriman <74624397+NilsFriman@users.noreply.github.com>
2024-11-19 17:07:52 +0100

    Lab 2

---
+++ laboration_2.py
@@ -0,0 +1,40 @@
+
+# Importerar de nödvändiga biblioteken
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Definerar funktionen
+def function(x):
+    return np.sin(x) * np.cos(x ** 2)
+
+
+def graph(a, b, f, n):
+
+    # Fixar data
+    x = np.linspace(a, b, n)
+    y = f(x)
+
+    # x och y är listor med värden, typ x = [0, 0.1, 0.2 ... ] och y = [5, 4.9, 4.8 ... ]
+
+    # Plottar grafen
+    plt.plot(x, y, color="green", linewidth=2)
+
+    # Fixar så den blir snygg etc
+    plt.axis('equal')
+    plt.grid('on')
+    plt.xlabel("X-axel")
+    plt.ylabel("Y-axel")
+    plt.title("Min supersexiga funktion")
+
+    # Visar grafen
+    plt.show()
+
+# Detta kan ignoreras
+def main():
+    if __name__ == "__main__":
+
+        # Vi kallar på funktionen!
+        graph(0, 3, function, 1000)
+
+
+main()
