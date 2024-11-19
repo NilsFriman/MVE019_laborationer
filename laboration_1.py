ff1e1524c736d61fd46748846bb4116293d89814
NilsFriman <74624397+NilsFriman@users.noreply.github.com>
2024-11-19 17:15:33 +0100

    Lab 1

---
+++ laboration_1.py
@@ -0,0 +1,42 @@
+
+# Importerar nödvändigt bibliotek
+import numpy as np
+
+# Definierar funktionen
+def function(x):
+    return np.sin(x)*np.cos(x**2)
+
+
+def Riemann(a, b, f, n):
+
+    # Bestämmer bredden på varje rektangel
+    width = np.abs(b - a) / n
+
+    # Definierar en summa
+    sum = 0
+
+    # Loopar över antalet rektanglar
+    for i in range(n):
+        # Lägger till rektangelns area till summan
+        # Detta beräknas som bredden gånger höjden
+        # Bredden finns i variabeln width
+        # Höjden hittas genom att ta funktionens värde vid det aktuella x värdet
+
+        # x värdet kommer att vara a (startvärdet) plus en halv (för att hamna mitt i en rektangel)
+        # plus bredden gånger i (antalet rektanglar vi redan lagt till i summan)
+        sum += f(a + i * width + width / 2) * width
+
+    return sum
+
+# Detta kan ignoreras (men är korrekt)
+def main():
+    if __name__ == "__main__":
+
+        # Loopar för att snyggt kunna printa våra integraler beroende på hur många rektanglar vi gör
+        # (Det behöver inte göras på ett såhär komplicerat sätt)
+        for i in range(6):
+            n = i * 500 + 1000
+            print("n =", str(n) + ":", Riemann(0, 3, function, n))
+
+
+main()
