f2882cb23212c4c2af90b2b88390358a8380b6b2
NilsFriman <74624397+NilsFriman@users.noreply.github.com>
2024-12-13 17:45:13 +0100

    Små ändringar

--- laboration_6.py
+++ laboration_6.py
@@ -25,26 +25,16 @@ def boring_bisect(f, a, b, tolerance):
         c = (a + b) / 2
     return c
 
-def recursive_newton(f, df, x, tolerance):
-    new_x = x - f(x) / df(x)
-    if abs(f(new_x)) < tolerance:
-        return new_x
-    else:
-        return recursive_newton(f, df, new_x, tolerance)
-
-def boring_newton(f, df, x0, tolerance):
-    x = x0 - f(x0) / df(x0)
+def newton(f, df, x, tolerance):
     while abs(f(x)) > tolerance:
         x = x - f(x) / df(x)
     return x
 
 # Uppgift 1
 for i in range(-1, -6, -1):
-    print("Recursive bisect for tolerance 10^" + str(i) + ":", recursive_bisect(fun, 0, 2, 10 ** i))
-    print("Boring bisect for tolerance 10^" + str(i) + ":", boring_bisect(fun, 0, 2, 10 ** i))
+    print("Recursive bisect with tolerance 10^" + str(i) + ":", recursive_bisect(fun, 0, 2, 10 ** i))
+    print("Boring bisect with tolerance 10^" + str(i) + ":", boring_bisect(fun, 0, 2, 10 ** i))
 
 # Uppgift 2
 for i in range(-1, -6, -1):
-    print("Recursive newton for tolerance 10^" + str(i) + ":", recursive_newton(fun, der_fun, 1.5, 10 ** i))
-    print("Boring newton for tolerance 10^" + str(i) + ":", boring_newton(fun, der_fun, 1.5, 10 ** i))
-
+    print("Newton method with tolerance 10^" + str(i) + ":", newton(fun, der_fun, 1.5, 10 ** i))
